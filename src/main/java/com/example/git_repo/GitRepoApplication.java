5e4ca19b3b84dc4f068fb4e72cf6fd26b97c002a
mssproject18 <msspro18@gmail.com>
2024-09-25 22:49:50 +0530

    push first commit

---
+++ src/main/java/com/example/git_repo/GitRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepoApplication.class, args);
+	}
+
+}
